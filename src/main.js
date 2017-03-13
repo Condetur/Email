e93eaa9a1977eedc57754de803574825d3dc7750
Morlauth <tino.caer@me.com>
2017-03-13 19:49:42 -0400

    Fixing

--- src/main.js
+++ src/main.js
@@ -16,6 +16,11 @@ var nodemailerMailgun = nodemailer.createTransport(mg(auth));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname));
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
 
 app.set('port', (process.env.PORT || 8000));
 
